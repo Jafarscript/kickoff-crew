02de61b1ea26f317d9aff7788992d7e1703546d7
Jafarscript <lihammedjafar@gmail.com>
2024-11-10 05:42:32 +0100

    target(_blank) add to the socials link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,16 @@ const Footer = () => {
     <div className='p-5 flex justify-between items-center flex-wrap bg-black text-white text-base'>
         <div>jafarscript</div>
         <div className="flex items-center gap-4">
-        <a href="https://github.com/Jafarscript">
+        <a href="https://github.com/Jafarscript" target='_blank'>
           <FaGithub  />
         </a>
-        <a href="https://www.linkedin.com/in/jafar-li-hammed-8817a91b4/">
+        <a href="https://www.linkedin.com/in/jafar-li-hammed-8817a91b4/" target='_blank'>
           <FaLinkedin  />
         </a>
-        <a href="https://web.facebook.com/jafar.olamide">
+        <a href="https://web.facebook.com/jafar.olamide" target='_blank'>
           <FaFacebookF  />
         </a>
-        <a href="https://www.linkedin.com/in/jafar-li-hammed-8817a91b4/">
+        <a href="https://www.linkedin.com/in/jafar-li-hammed-8817a91b4/" target='_blank'>
           <FaTwitter  />
         </a>
       </div>
